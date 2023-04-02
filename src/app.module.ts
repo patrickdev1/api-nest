1369ea1792ccce6a5f4f0fdb4073f411fef3be6d
Patrick <patrickamaralr1@gmail.com>
2023-04-02 17:48:14 -0300

    feat: conexao banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODBLINK),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
